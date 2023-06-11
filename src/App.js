e654b25b09e331b907c7200d0429765059ab71dd
Victory Odumeh <vodumeh@gmail.com>
2023-06-11 22:09:54 +0100

    loading...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Feed from './Components/Feed/Feed';
+import Rightbar from './Components/Rightbar/Rightbar';
+import Leftbar from './Components/Leftbar/Leftbar';
+import Topbar from './Components/Topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
-      <Feed></Feed>
+      <>
+        <Topbar></Topbar>
+        <Leftbar></Leftbar>
+        <Feed></Feed>
+        <Rightbar></Rightbar>
+      </>
     </div>
   );
 }
---
+++ src/Components/Rightbar/Rightbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rightbar() {
+  return (
+    <div>
+      <h1>Rightbar</h1>
+    </div>
+  )
+}
+
+export default Rightbar;
