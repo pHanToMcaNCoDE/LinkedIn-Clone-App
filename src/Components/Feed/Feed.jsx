55b890256b23a120dfa398e2149745c15e1b34eb
Victory Odumeh <vodumeh@gmail.com>
2023-06-12 23:14:22 +0100

    starting with feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -4,6 +4,40 @@ import './feed.css';
 function Feed() {
   return (
     <div className='feed'>
+      <div className='post'>
+        <div className='top'>
+          <img src='/assets/acc.png' alt='dp profile'></img>
+          <input itemType='submit' placeholder='Start a post'></input>
+        </div>
+        <div className='bottom'>
+          <ul>
+            <li>
+              <a href=''>
+                <i class="fa-solid fa-image"></i> 
+                <p>Photo</p>
+              </a>
+            </li>
+            <li>
+              <a href=''>
+                <i class="fa-brands fa-youtube"></i> 
+                <p>Video</p>
+              </a>
+            </li>
+            <li>
+              <a href=''>
+                <i class="fa-solid fa-calendar-week"></i>
+                <p>Event</p>
+              </a>
+            </li>
+            <li>
+              <a href=''>
+                <i class="fa-solid fa-newspaper"></i>
+                <p>Write article</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
         <h1>Feed</h1>
     </div>
   )
