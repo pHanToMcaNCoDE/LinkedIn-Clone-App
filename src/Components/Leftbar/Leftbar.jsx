dbe4dd8a93b11487edea67d0f589c6db89112e1f
Victory Odumeh <vodumeh@gmail.com>
2023-06-12 10:19:24 +0100

    moving to the body of the site

--- src/Components/Leftbar/Leftbar.jsx
+++ src/Components/Leftbar/Leftbar.jsx
@@ -8,19 +8,28 @@ function Leftbar() {
         <div className='box'>
           <div className='top'>
             <div className='images'>
-              <img src='' alt='Dp'></img>
-              <img src='' alt='Profile picture'></img>
+              <img className='bg' src='/assets/bg.png' alt='Dp'></img>
+              <img className='dp' src='/assets/acc.png' alt='Profile picture'></img>
             </div>
             <div className='txt'>
               <h2>Victory Odumeh</h2>
               <p>Full-Stack Developer</p>
             </div>
           </div>
-          <div className='mid-one'>
-            
-          </div>
-          <div className='mid-two'>
-            
+          <div className='mid'>
+            <div className='stats'>
+              <div className='align'>
+                <a href=''>Who's viewed your profile?</a>
+                <p>15</p>
+              </div>
+              <div className='align'>
+                <a href=''>Impressions of your post</a>
+                <p>6</p>
+              </div>
+            </div>
+            <div className='stats'>
+              
+            </div>
           </div>
           <div className='btm'>
             
